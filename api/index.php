6b69404f608b8f9f03075c98dae59dbd29874da4
rakaXPPL2 <rakaraditya4750@gmail.com>
2026-09-03 10:06:59 +0700

    apiindex

--- api/index.php
+++ api/index.php
@@ -13,6 +13,8 @@ putenv('LOG_CHANNEL=errorlog');
 
 require __DIR__ . '/../vendor/autoload.php';
 
+require __DIR__ . '/../public/index.php';
+
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 $storagePath = '/tmp/laravel-storage';
 
