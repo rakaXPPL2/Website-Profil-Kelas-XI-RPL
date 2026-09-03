8a9ee6e5d0b56f85233b8c73954b2c003e9d31ac
rakaXPPL2 <rakaraditya4750@gmail.com>
2026-09-03 12:34:32 +0700

    Restore Blade provider registration

--- api/index.php
+++ api/index.php
@@ -37,6 +37,7 @@ if (!is_dir($storagePath)) {
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 $app->useStoragePath($storagePath);
+$app->register(\Illuminate\View\ViewServiceProvider::class);
 
 try {
 	$response = $app->handleRequest(Illuminate\Http\Request::capture());
