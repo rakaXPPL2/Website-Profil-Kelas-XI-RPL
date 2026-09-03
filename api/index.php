3fb2c8b1f4eadfb4e42e13d1293c7bac5e8c85ba
rakaXPPL2 <rakaraditya4750@gmail.com>
2026-09-03 10:32:21 +0700

    apiindex

--- api/index.php
+++ api/index.php
@@ -1,28 +1,29 @@
 <?php
 
 if (!isset($_ENV['APP_KEY']) && !getenv('APP_KEY')) {
-	putenv('APP_KEY=base64:exWOZeauMvmVFWddAkyDbZY/Au9l3NDCFAHLa7ENRD0=');
-	$_ENV['APP_KEY'] = 'base64:exWOZeauMvmVFWddAkyDbZY/Au9l3NDCFAHLa7ENRD0=';
+    putenv('APP_KEY=base64:j0JsJx97dN30OJmXhAGGU561GCNa4GXwYZw6Dr/dt68=');
+    $_ENV['APP_KEY'] = 'base64:j0JsJx97dN30OJmXhAGGU561GCNa4GXwYZw6Dr/dt68=';
 }
 
 putenv('APP_ENV=production');
 putenv('APP_DEBUG=false');
 putenv('SESSION_DRIVER=array');
+putenv('SESSION_CONNECTION=');
 putenv('CACHE_STORE=array');
+putenv('CACHE_PREFIX=');
+putenv('QUEUE_CONNECTION=sync');
 putenv('LOG_CHANNEL=errorlog');
 
 require __DIR__ . '/../vendor/autoload.php';
-
-require __DIR__ . '/../public/index.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 $storagePath = '/tmp/laravel-storage';
 
 if (!is_dir($storagePath)) {
-	mkdir($storagePath . '/framework/cache/data', 0755, true);
-	mkdir($storagePath . '/framework/sessions', 0755, true);
-	mkdir($storagePath . '/framework/views', 0755, true);
-	mkdir($storagePath . '/logs', 0755, true);
+    mkdir($storagePath . '/framework/cache/data', 0755, true);
+    mkdir($storagePath . '/framework/sessions', 0755, true);
+    mkdir($storagePath . '/framework/views', 0755, true);
+    mkdir($storagePath . '/logs', 0755, true);
 }
 
 $app->useStoragePath($storagePath);
